e381656df1102ca9427663d019acc3ef51f8fda5
Yusuf ARSLAN <arslanon@outlook.com>
2021-02-03 22:32:32 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+
+    //Generic Constraint Generic kısıtlama // T bir class bir referans tipi olacak şekilde kısıtlandı
+    //Herhangi bir class da olamasın IEntitiy olsun
+    //class : referans tipi
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+    //new() : New lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntitiy,new()
+    {
+
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Db tabloları ile proje classşarını bağlama
+    public class NorthwindContext:DbContext
+    {
+    }
+}
